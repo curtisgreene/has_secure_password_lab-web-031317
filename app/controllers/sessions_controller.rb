47a2dc4a266ab44f4536fe976e1e48711ca3ea6a
curtisgreene <curtis.j.w.greene@gmail.com>
2017-04-13 17:42:06 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,16 @@ class SessionsController < ApplicationController
   def create
     if params[:user][:password].present? && params[:user][:name].present?
       @user = User.find_by(name: params[:user][:name])
-      if @user.authenticate(params[:user][:password])
+      if @user.try(:authenticate, params[:user][:password])
         session[:user_id] = @user.id
+        flash[:message] = "Good job"
         redirect_to homepage_path(@user)
       else
+        flash[:message] = "Go away"
         redirect_to login_path
       end
     else
+      flash[:message] = "Go away"
       redirect_to login_path
     end
   end
